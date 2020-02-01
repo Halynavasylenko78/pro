9d91afab6701cec9c11da930a073edb1c2ea7f87
yarik <yariks@i.ua>
2020-02-01 11:26:22 +0200

    commit

--- src/test/java/com/it/helpers/LoginHelper.java
+++ src/test/java/com/it/helpers/LoginHelper.java
@@ -5,6 +5,7 @@ import com.it.users.User;
 
 public class LoginHelper extends LoginPage {
     public void login(User user) {
+        log.info(String.format("User login username - %s, password - %s .",user.userName,user.password));
         login(user.userName,user.password);
     }
 }
